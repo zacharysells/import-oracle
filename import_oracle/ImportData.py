aaaeede3df01d708422c687e6b1ea02e543944b7
Zachary Sells <zacharysells@gmail.com>
2021-09-02 13:26:13 -0700

    Revising logging. Allowing wildcard CSV. Continuing after errors

--- import_oracle/ImportData.py
+++ import_oracle/ImportData.py
@@ -17,11 +17,18 @@ parser.add_argument("json_file_path", help="Relative path to file or file glob o
 parser.add_argument("--executesql", help="`json_file_path` arg will be parsed as an ExecuteSQL script.", action="store_true")
 parser.add_argument("--selectall", help="Print all rows in table", action="store_true")
 parser.add_argument("--bootstrap", help="Bootstrap db with provided .sql file. WARNING this will drop the table first.")
+parser.add_argument("--loglevel", help="Log level. Allowed values are [DEBUG, INFO, WARN, ERROR]. Defaults to INFO", default="INFO")
 args = parser.parse_args()
 
+loglevel_mapping = {
+    'DEBUG': logging.DEBUG,
+    'INFO': logging.INFO,
+    'WARN': logging.WARN,
+    'ERROR': logging.ERROR
+}
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
-    level=logging.INFO,
+    level=loglevel_mapping[args.loglevel],
     datefmt='%Y-%m-%d %H:%M:%S'
 )
 
@@ -30,7 +37,7 @@ oracle_strftime_fmt = 'MM/DD/YY HH24:MI:SS'
 script_execution_time = datetime.datetime.now().strftime(date_strftime_fmt)
 
 def execute_sql(connection, query, commit=True, rollback_transaction=False):
-    logging.info('Executing SQL query="%s"' % query)
+    logging.debug('Executing SQL query="%s"' % query)
     curs = connection.cursor()
     try:
         curs.execute(query)
@@ -46,9 +53,8 @@ def execute_sql(connection, query, commit=True, rollback_transaction=False):
 def executemany_sql(connection, db_table, fields, rows, commit=True):
     inserts = '\n'.join(['into %s(%s) values(%s)' % (db_table, ','.join(fields), row) for row in rows])
     sql_query = """insert all %s SELECT * FROM dual""" % inserts
-    logging.info('Bulk SQL query="%s"' % sql_query)
+    logging.debug('Bulk SQL query="%s"' % sql_query)
     try:
-        pass
         curs = connection.cursor()
         curs.execute(sql_query)
         connection.commit()
@@ -62,7 +68,7 @@ def execute_select_sql(connection, query):
     This function will execute the provided `query` and return the results as a list of rows.
     The first row in the results list will be the column names.
     """
-    logging.info('Executing SQL query="%s"' % query)
+    logging.debug('Executing SQL query="%s"' % query)
     curs = connection.cursor()
     curs.execute(query)
     column_alias = [c[0] for c in curs.description]
@@ -111,7 +117,8 @@ def convert_xlsx_to_csv(filename):
         csv.write('\n')
     return csv.name
 
-def map_mapping_types(row, mapping_list, header=True, header_values=None):
+def map_mapping_types(row, mapping_list, row_num=None, source_filename=None, header=True, header_values=None):
+    row += [''] * (len(header_values) - len(row))
     row_string = ''
     for i, elem in enumerate(mapping_list):
         e = elem.get('Type')
@@ -121,6 +128,12 @@ def map_mapping_types(row, mapping_list, header=True, header_values=None):
             row_string = "%s,'%s'" % (row_string, e.replace('C-', ''))
         elif e == 'S-datetime.now()':
             row_string = "%s,TO_DATE('%s', '%s')" % (row_string, script_execution_time, oracle_strftime_fmt)
+        elif e.startswith('S-FILENAME'):
+            _, i, e = tuple(e.split('.'))
+            value = os.path.basename(source_filename)[int(i)-1:int(e)]
+            row_string = "%s,'%s'" % (row_string, value)
+        elif e == 'S-ROWNUM':
+            row_string = "%s,'%s'" % (row_string, row_num)
         else: # Default to 'S' column type.
             if header:
                 source_column = int(header_values.index(elem['Source']))
@@ -259,33 +272,41 @@ def process_import(descriptor_file):
         return
 
     import_file_path = os.path.join(os.path.dirname(__file__), source_file)
-    if source_filetype == 'xlsx':
-        logging.info('Converting %s to temporary csv' % import_file_path)
-        source_file = convert_xlsx_to_csv(import_file_path)
-        logging.info('Temporary csv created - %s' % source_file)
+    for import_file in glob.glob(import_file_path):
+        logging.info("Processing %s" % import_file)
 
-    sorted_col_mappings = sorted(descriptor_file_data['ColMappings'], key=lambda k: int(k.get('Order') or sys.maxsize))
-    header_values = None
-    regex_pattern = re.compile(r'''((?:[^%s"]|"[^"]*"|'[^']*')+)''' % source_delimeter)
-    with open(os.path.join(os.path.dirname(__file__), source_file), 'r') as f:
-        if source_fileheader:
-            header_values = next(f)
-            header_values = regex_pattern.split(header_values.strip())[1::2]
-            header_values = ['' if i == ' ' else i for i in header_values]
-        while True:
-            bulk_row_insert = []
-            lines = f.readlines(MAX_BYTES_PER_CHUNK)
-            if not lines:
-                break
-            logging.info("Reading %d bytes from input file - %d rows" % (MAX_BYTES_PER_CHUNK, len(lines)))
-            for line in lines:
-                data = regex_pattern.split(line.strip())[1::2]
-                data = ['' if i == ' ' else i for i in data]
-                bulk_row_insert.append(map_mapping_types(data, sorted_col_mappings, header=source_fileheader, header_values=header_values))
-            logging.info('Bulk inserting %d rows' % len(bulk_row_insert))
-            executemany_sql(connection, db_table, [x['Target'] for x in sorted_col_mappings], bulk_row_insert)
+        if source_filetype == 'xlsx':
+            logging.info('Converting %s to temporary csv' % import_file)
+            source_file = convert_xlsx_to_csv(import_file)
+            logging.info('Temporary csv created - %s' % source_file)
+
+        sorted_col_mappings = sorted(descriptor_file_data['ColMappings'], key=lambda k: int(k.get('Order') or sys.maxsize))
+        header_values = None
+        regex_pattern = re.compile(r'''%s(?=(?:[^"]*"[^"]*")*[^"]*$)''' % source_delimeter)
+        with open(import_file, 'r') as f:
+            if source_fileheader:
+                header_values = next(f)
+                header_values = regex_pattern.split(header_values.strip())
+                header_values = ['' if i == ' ' else i for i in header_values]
+            row_counter=1
+            while True:
+                row_counter_start=row_counter
+                bulk_row_insert = []
+                lines = f.readlines(MAX_BYTES_PER_CHUNK)
+                if not lines:
+                    break
+                logging.info("Reading %d bytes from input file - %d row(s)" % (MAX_BYTES_PER_CHUNK, len(lines)))
+                for line in lines:
+                    data = regex_pattern.split(line.strip())
+                    data = ['' if i == ' ' else i for i in data]
+                    bulk_row_insert.append(map_mapping_types(data, sorted_col_mappings, row_num=row_counter, source_filename=source_file, header=source_fileheader, header_values=header_values))
+                    row_counter += 1
+                logging.info('Bulk inserting rows %d-%d' % (row_counter_start, row_counter-1))
+                try:
+                    executemany_sql(connection, db_table, [x['Target'] for x in sorted_col_mappings], bulk_row_insert)
+                except cx_Oracle.DatabaseError as e:
+                    pass
 
 if __name__ == "__main__":
     for desc_file in glob.glob(args.json_file_path):
-        logging.info("Processing %s" % desc_file)
         process_descriptor_file(desc_file)
